a54374ac7392e62d898cf6993bb373aea45af9a9
Pankajkumar2608 <tarachandji104@gmail.com>
2025-12-30 10:11:30 +0530

    added normalize function and gethtmlfrombody

--- crawel.test.js
+++ crawel.test.js
@@ -1,4 +1,5 @@
-const { normalizeUrl } =  require("./crawl");
+const { normalizeUrl, getURLsFromHTML } = require("./crawl");
+const { JSDOM } = require("jsdom");
 const  { test,expect } = require("@jest/globals"); 
 
 test("normalizeUrl", () => {
@@ -24,5 +25,13 @@ test("normalizeUrl http", () => {
   const input = "http://Blog.Boot.Dev/path/";
   const actual = normalizeUrl(input);
   const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+test("getURLsFromHTML absolute", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/"];
   expect(actual).toEqual(expected);
 });
